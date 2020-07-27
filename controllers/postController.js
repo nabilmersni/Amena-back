96c65e9919c7517fd1cd4e98909b052fcce9932f
nabilmersni <nabilmersni123@gmail.com>
2020-07-27 14:40:46 +0100

    create the get fundrainsing detail and my fundrainsing api

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,4 +24,26 @@ app.get('/all',async (req,res) =>{
     }
 })
 
+app.get('/myPost/:id',async (req,res) =>{
+    try {
+        let id = req.params.id;
+        let posts = await Post.find({userId: id});
+        res.status(200).send(posts);
+
+    } catch (error) {
+        res.status(400).send({message: "error !"})
+    }
+})
+
+app.get('/detail/:id', async (req,res) =>{
+    try {
+        let id = req.params.id;
+        let post = await Post.findOne({_id : id});
+        res.status(200).send(post);
+        
+    } catch (error) {
+        res.status(400).send({message: "error !"})
+    }
+})
+
 module.exports = app;
